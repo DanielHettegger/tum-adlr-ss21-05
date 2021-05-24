51cb0b11784ef715f9cbb836cde9c41a92d9b4c4
Daniel Hettegger <danielhettegger@gmail.com>
2021-05-24 13:45:47 +0200

    Updated to load model and continue training

--- train_td3.py
+++ train_td3.py
@@ -15,8 +15,10 @@ env = gym.make('kuka_iiwa_insertion-v0', use_gui=False)
 # The noise objects for TD3
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-
-model = TD3(MlpPolicy, env, action_noise=action_noise, verbose=1)
+try:
+    model = TD3.load("kuka_iiwa_insertion-v0",env, action_noise=action_noise, verbose=1)
+except e:
+    model = TD3(MlpPolicy, env, action_noise=action_noise, verbose=1)
 model.learn(total_timesteps=100000, log_interval=10)
 model.save("kuka_iiwa_insertion-v0")
 
