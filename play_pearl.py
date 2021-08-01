fea6dc837946ac0b74891a8f624d049a85173cc7
Daniel Hettegger <danielhettegger@gmail.com>
2021-08-01 13:31:15 +0200

    play pearl

--- play_pearl.py
+++ play_pearl.py
@@ -1,5 +1,7 @@
 import gym
 import numpy as np
+from stable_baselines3.common.callbacks import EventCallback
+import torch as th
 
 import kuka_iiwa_insertion
 
@@ -8,24 +10,66 @@ from stable_baselines3 import PEARL
 
 def play():
     env = gym.make('kuka_iiwa_insertion-v0', use_gui=True)
-    model = PEARL.load("models/kuka_iiwa_insertion-v0_pearl_best_model", env=env)
+    model = PEARL.load("models/feasible-dust-68_latest_model", env=env)
 
-    obs = env.reset()
+    model.callback = EventCallback()
+
+
     i = 0
-    episode_reward = 0.0
     while True:
-        i += 1
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        episode_reward += rewards
-        if i % 10 == 0 or dones: 
-            print(obs, episode_reward, rewards, info)
-        if dones:
-            print("="*20 + " RESET " + "="*20)
-            episode_reward = 0
-            obs = env.reset_task(np.random.randint(0,3))
+        task_idx = i%env.number_of_tasks
+        reward = eval_task(model, model.env, task_idx)
+        print("Task {} Average reward was {}".format(task_idx, reward))
+        i+=1
 
-        #env.render()
+
+
+
+def eval_task(model, env, task_idx):
+    print("Evaluating model in env with task idx {}".format(task_idx))
+    with th.no_grad():
+        env.env_method("reset_task", task_idx)
+
+        model.actor.clear_z()
+        num_transitions = 0
+        num_trajs = 0
+
+        model.JUST_EVAL.reset()
+        n_trajs = 10
+        while num_transitions < 1000 * n_trajs and num_trajs <= n_trajs:
+            num = model.obtain_samples(
+                deterministic=True,
+                max_samples=1000 ,#- num_transitions,
+                max_trajs=1,
+                accum_context=True,
+                replaybuffers=[model.JUST_EVAL],
+            )
+            num_transitions += num
+            num_trajs += 1
+            model.actor.infer_posterior(model.actor.context)
+
+        # Collect reward with infered posterior
+        model.JUST_EVAL.reset()
+
+        print("Posterior initialized. Now collecting reward data.")
+
+        num_transitions = 0
+        num_trajs = 0
+        n_trajs = 3
+        while num_transitions < 1000 * n_trajs and num_trajs <= n_trajs:
+            num = model.obtain_samples(
+                deterministic=True,
+                max_samples=1000 ,#- num_transitions,
+                max_trajs=1,
+                accum_context=True,
+                replaybuffers=[model.JUST_EVAL],
+            )
+            num_transitions += num
+            num_trajs += 1
+
+
+        rwd = model.JUST_EVAL.rewards[range(model.JUST_EVAL.pos)]
+        return (np.sum(rwd) / n_trajs)
 
 if __name__ == "__main__":
     play()
